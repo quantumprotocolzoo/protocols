04534d156dbb1c21acc6fd177c89ee3aca741d80
Axel Dahlberg <axel.dahlberg12@gmail.com>
2019-03-27 11:40:10 +0100

    Moved examples of how to use the python lib from SimulaQron repo to here

---
+++ examples/pythonLib/butterfly/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+python3 node_S1.py &
+python3 node_S2.py &
+python3 node_R1.py &
+python3 node_R2.py &
+python3 node_T1.py &
+python3 node_T2.py &
---
+++ examples/pythonLib/wstate/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3 default_Wstate_transmitter.py Node0 &
+python3 default_Wstate_receiver.py Node1 &
+python3 default_Wstate_receiver.py Node2 &
+python3 default_Wstate_receiver.py Node3 &
+python3 default_Wstate_receiver.py Node4 &
+python3 default_Wstate_receiver.py Node5 &
+python3 default_Wstate_receiver.py Node6 &
