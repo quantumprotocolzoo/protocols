3b82c9c235a51630eab65dd90f494936e1756331
apassenger <ustungozde@gmail.com>
2019-05-21 17:43:28 +0300

    Update nPartyCoinFlip.py

--- examples/pythonLib/coinflipLeader/nPartyCoinFlip.py
+++ examples/pythonLib/coinflipLeader/nPartyCoinFlip.py
@@ -3,24 +3,27 @@ from cqc.pythonLib_protocols.coinflip_leader import CoinflipConsensus
 
 def main():
 
-    leaderChooser = CoinflipConsensus(arr)
+    leaderChooser = CoinflipConsensus(arr)  # Elects a leader from array that you declared
     return leaderChooser.leader()
 
-
-
-arr = []
-veri = input("Add person")
-while veri:
+# For 12-13-14-15 lines: After seeing "Add person" sentence, you can add person that how many person you want to add.
+#  After adding one person you should press "enter" and then you can continue to add person with press enter 
+#  If you want to cancel from adding person you should press enter again
+# After enter the all name that you wanted to add you should press "enter"
+# Note that: you should use name from cqc's names space such as Alice, Bob, David, Eve Charlie.., you cant't use name randomly such as Gozde, Axel ...
+arr = []  # Here an empty array was defined
+veri = input("Add person")   
+while veri:                     
     arr.append(veri)
     veri = input("Add person")
         
 
-
+# giving a value for each leader
 d = dict()
 for veri in arr:
     d[veri] = 0
     
-
+# Runs 20 rounds of leader election and prints the results.
 for i in range(0, 20):
     if i % 10 == 0:
         print(i)
